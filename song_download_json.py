e397b8de683bdac7ed2cbeb6e81727dfcee909e8
kimsangwon <tkddnjs2014@naver.com>
2019-04-08 11:52:45 +0900

    v2.0

--- song_download_json.py
+++ song_download_json.py
@@ -22,35 +22,64 @@ print('Soul-RB')
 print('Spoken')
 
 genre=input('input genre: ')
+number=input('input song number: ')
 
-with open('{}_chart.json'.format(genre)) as json_file:
-	json_data= json.load(json_file)
-'''
-	# 문자열
-	# key가 json_string인 문자열 가져오기
-json_string = json_data["json_string"]
-print(json_string)
-'''
-	# 숫자
-	# key가 json_number인 숫자 가져오기
-json_number = json_data["json_number"]
-print(str(json_number)) # 숫자이기 때문에 str()함수를 이용
-'''
 
-	# 배열
-	# key가 json_array인 배열 가져오기
-json_array=json_data["json_array"]
-print(json_array)
-'''
 
-	# 객체
-	# key가 json_object인 객체 가져와서 만들기
-	# json object의 경우에 python ojbect로 바꿀때는 따로 처리를 해줘야합니다.
-	# 기본형은 dictionary입니다.
-json_object = json_data["json_object"]
-print(json_object)
+def fma_Download(html):
+	temp_list = []
+	div_list = html.find_all('div', {'class': 'play-item'})
+	
+	for div in div_list :
+		down1= div.find('span', {'class': 'playicn'}).find("a")
+		down2=str(down1)
+		down3=down2[27:111]
 
-	# bool형
-	# key가 json_bool인 bool형 자료 가져오기
-json_bool = json_data["json_bool"]
-print(json_bool)
+		temp_list.append([down3])
+
+	return temp_list
+
+def download(url, number):
+	file_name = url.split('/')[-1]
+	name= number+".mp3"
+	print(name)
+
+	with open(name, "wb") as file:
+			response = requests.get(url)
+			file.write(response.content)
+
+
+if __name__ == '__main__':
+
+	fma_download_list=[]
+	fma_list=[]
+
+	req1 = requests.get('https://freemusicarchive.org/genre/{}/?sort=track_date_published&d=1&page=1&per_page=200/'.format(genre))
+
+	source1 = req1.text
+	html2 = BeautifulSoup(source1, 'lxml')
+
+	final_page2=html2.select('a[href^="https://freemusicarchive.org/genre/{}/?sort=track_date_published&d=1&page="]'.format(genre))
+	final_page=final_page2[6].text
+	final_page=int(final_page)
+
+	final_song2=html2.find('div', {'class': 'pagination-full'}).find_all("b")
+	final_song=final_song2[2].text
+	final_song=int(final_song)
+
+	with open('{}_chart.json'.format(genre)) as json_file:
+		json_data= json.load(json_file)
+
+	json_string = json_data[number]
+	print(json_string)
+
+	for page in range(0,final_page+1):
+		req = requests.get('https://freemusicarchive.org/genre/{}/?sort=track_date_published&d=1&page={}&per_page=200'.format(genre, page))
+		source = req.text
+		html = BeautifulSoup(source, 'lxml')
+		fma_download_list += fma_Download(html)
+
+	num2=int(number)-1
+	url=" ".join(fma_download_list[num2])
+	print(url)
+	download(url,number)
